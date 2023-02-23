ea06279a5eeea19ed6bb314e5a1584a21fba2c19
knechtma <knechtma@student.ethz.ch>
2023-02-23 17:09:27 +0100

    UDP communication working

--- examples/faive_hand/faive_hand_teensy/faive_hand_teensy.ino
+++ examples/faive_hand/faive_hand_teensy/faive_hand_teensy.ino
@@ -3,6 +3,11 @@
 #include "pb_encode.h"
 #include "pb.h"
 
+#include <QNEthernet.h>
+#include <QNEthernetUDP.h>
+
+using namespace qindesign::network;
+
 //string encode decode to pb
 bool encode_string(pb_ostream_t* stream, const pb_field_t* field, void* const* arg)
 {
@@ -16,11 +21,48 @@ bool encode_string(pb_ostream_t* stream, const pb_field_t* field, void* const* a
 
 
 uint8_t buffer[1024] = {'\0'};
-pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer)); // what does this do?
+pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+IPAddress ip{192, 168, 0, 143};   // Unique IP
+IPAddress sn{255,255,255,0};  // Subnet Mask
+IPAddress gw{192, 168, 0, 1};       // Default Gateway
+
+unsigned int localPort = 5000;      // local port to listen on
+const int UDP_TX_PACKET_MAX_SIZE = 1024;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // buffer to hold incoming packet
+String datReq; // String to hold incoming packet
+int packetSize; // Size of incoming packet
+EthernetUDP Udp; // A UDP instance to let us send and receive packets over UDP
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  if (!Ethernet.begin(ip,sn,gw)) {
+    Serial.println("Failed to start Ethernet\n");
+    return;
+  }
+  Udp.begin(localPort);
+
+  uint8_t mac[6];
+  Ethernet.macAddress(mac);  // Retrieve the MAC address and print it out
+  Serial.printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\n",
+         mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  
+  // Show whether a cable is plugged in or not.
+  Ethernet.onLinkState([](bool state) {
+    Serial.printf("[Ethernet] Link %s\n", state ? "ON" : "OFF");
+  });
+
+  Serial.print("Teensy is at ");
+  Serial.println(Ethernet.localIP());
+
+  delay(1500);
+
+
 
   stream.bytes_written = 0;
 
@@ -98,20 +140,43 @@ void setup() {
 
   // Serial.print("Message: ");
 
-  for(int i = 0; i<stream.bytes_written; i++){
-    Serial.printf("%02X",buffer[i]);
-  }
-  Serial.println();
-
+//   Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+//   for(int i = 0; i<stream.bytes_written; i++){
+//     Udp.write(buffer[i]);
+//     // Serial.printf("%02X",buffer[i]);
+//   }
+//   // Serial.println();
+//   Udp.endPacket();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Writing to UDP");
+
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
   for(int i = 0; i<stream.bytes_written; i++){
-    Serial.printf("%02X",buffer[i]);
+    Udp.write(buffer[i]);
+    // Serial.printf("%02X",buffer[i]);
   }
-  Serial.println();
-  delay(500);
+  // Serial.println();
+  Udp.endPacket();
+
+  // for(int i = 0; i<stream.bytes_written; i++){
+  //   Serial.printf("%02X",buffer[i]);
+  // }
+  // Serial.println();
+  delay(100);
+
+  // read data from UDP
+  packetSize = Udp.parsePacket();
+  if (packetSize > 0) {
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    String datReq(packetBuffer);
+    Serial.println(datReq);
+  }
+  memset(packetBuffer, 0, UDP_TX_PACKET_MAX_SIZE);
+
+  delay(100);
 
   
 }
